a1fd0ca1d0406659c98471d44faeae1d9820905a
Idowu Helen <idowuhelenoluwatosin@gmail.com>
2024-09-20 12:23:42 +0100

    project completed

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import {
  import AddJob from './pages/AddJob';
  import NotFoundPage from './pages/NotFoundPage';
  import JobPage, {jobLoader} from './pages/JobPage';
+ import EditJobPage from './pages/EditJobPage';
 
  
 
@@ -23,22 +24,52 @@ const App = () => {
       },
       body:JSON.stringify(newJob),
     });
+    return;
    };
   
    //Delete Job
-   const deleteJob =async (id) => {
-    const res = await fetch (`api/jobs/${id}`, {
+   const deleteJob = async (id) => {
+    const res = await fetch (`/api/jobs/${id}`, {
       method: 'DELETE'
     });
+    return;
+   };
+
+
+   //Update Job
+   const updateJob = async(job) => {
+    const res = await fetch (`/api/jobs/${job.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body:JSON.stringify(job),
+    });
+    return;
    }
+
   const router = createBrowserRouter(
     createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage/>} />
       <Route path='/jobs' element={<JobsPage/>} />
-      <Route path='/add-job' element={<AddJob addJobSubmit={addJob}/>} />
-       <Route path='/jobs/:id' element={<JobPage deleteJob={ deleteJob }/>}  loader={jobLoader}/> 
+      <Route 
+        path='/add-job' 
+        element={<AddJob addJobSubmit={addJob}/>} 
+      />
+       <Route 
+        path='/edit-job/:id' 
+        element={<EditJobPage updateJobSubmit={updateJob}/>}  
+        loader={jobLoader}
+      /> 
+       <Route 
+        path='/jobs/:id' 
+        element={<JobPage deleteJob={ deleteJob }/>}  
+        loader={jobLoader}
+      /> 
+
       <Route path='*' element={<NotFoundPage />} />
+      
     </Route>
      )
   )
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,4 +1,6 @@
 import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+// import 'react-toastify/ReactToastify.css';
 import Navbar from '../components/Navbar';
 
 const mainLayout = () => {
